fad54ec364b4b88d2493b7da8fde34c1a7a5f0b3
Nate Derbinsky <nate.derbinsky@gmail.com>
2016-09-10 16:28:37 -0400

    learning all the things

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
 from flask import Flask, request, redirect
 app = Flask(__name__)
 
+def ssl_enabled(f):
+    
+    def wrapper(name):
+        if request.headers.get('X-Forwarded-Proto') == "http":
+            return redirect(request.url.replace('http://', 'https://', 1), code=301)
+        else:
+            return f(name)
+            
+    return wrapper
+
+@ssl_enabled
+def _hello(name):
+    return "Hello {}".format(name)
+
 @app.route('/')
 def hello():
-    proto = request.headers.get('X-Forwarded-Proto')
-    if proto == "http":
-        return redirect(request.url.replace('http://', 'https://', 1), code=301)
-    
-    return "Hello World!"
+    return _hello("World")
     
 @app.route('/<name>')
 def hello_name(name):
-    return "Hello {}!".format(name)
+    return _hello(name)
 
 if __name__ == '__main__':
     app.run()
